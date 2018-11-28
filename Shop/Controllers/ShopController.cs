8949c867b644b6e0879048e331a065de22daf8a4
Easy-Zhou <401700108@qq.com>
2018-11-28 10:15:52 +0800

    move project to dotNetLab

---
+++ Shop/Controllers/ShopController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            Shop.Models.ShopEntities db = new Models.ShopEntities();
+            List<Shop.Models.T_Shop_Category> lst = db.T_Shop_Category.ToList();
+            ViewBag.lst = lst;
+            return View();
+        }
+
+        public ActionResult List(int CategoryId=1)
+        {
+
+            Shop.Models.ShopEntities db = new Models.ShopEntities();
+            Shop.Models.T_Shop_Category category = db.T_Shop_Category.Single(m => m.Id == CategoryId);
+            List<Shop.Models.T_Shop_Product> lst = category.T_Shop_Product.ToList();
+            ViewBag.lst = lst; 
+            return View();
+        }
+    }
+}
---
+++ lab3/Controllers/ShopController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab3.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            lab3.Models.ShEntities db = new Models.ShEntities();
+            List<lab3.Models.T_Shop_Category> lst = db.T_Shop_Category.ToList();
+            ViewBag.lst = lst;
+            return View();
+        }
+    }
+}
