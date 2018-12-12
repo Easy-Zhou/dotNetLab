2eb9f5378cbb2d499f0dcd61e34c9837a20dc015
Easy-Zhou <401700108@qq.com>
2018-12-12 21:23:36 +0800

    添加了管理员端
    添加了area管理员端，同时对content中的目录进行了调整，将图片统一放入img文件夹，并引入sb-admin管理员端模版，并且在admin端新建了layout 使用sb-amin 模版并进行适当的修改，同时对 jquery.dataTables.min.js 进行部分汉化

---
+++ Shop/Areas/admin/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Areas.admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: admin/Category
+        public ActionResult Index()
+        {
+            Shop.Models.ShopEntities db = new Models.ShopEntities();
+            List<Shop.Models.T_Shop_Category> cates = db.T_Shop_Category.ToList();
+            ViewBag.lst = cates;
+            return View();
+        }
+    }
+}
