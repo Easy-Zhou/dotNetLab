3e5e3c05211d1d858af2e95f7307f599462e441b
Easy-Zhou <401700108@qq.com>
2018-12-13 22:37:56 +0800

    2018.12.13修改
    增加了管理员端分类添加功能，初步完成图片上传的功能，同时更改了管理端 分类管理下的标签及其href

--- Shop/Areas/admin/Controllers/CategoryController.cs
+++ Shop/Areas/admin/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,6 +15,26 @@ namespace Shop.Areas.admin.Controllers
             Shop.Models.ShopEntities db = new Models.ShopEntities();
             List<Shop.Models.T_Shop_Category> cates = db.T_Shop_Category.ToList();
             ViewBag.lst = cates;
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(HttpPostedFileBase file,String categoryName)
+        {
+            var fileName = file.FileName;
+            var filePath = Server.MapPath("~/Content/img");
+            file.SaveAs(Path.Combine(filePath,fileName));
+            Shop.Models.ShopEntities db = new Models.ShopEntities();
+            Shop.Models.T_Shop_Category cates = new Models.T_Shop_Category();
+            cates.CategoryName = categoryName;
+            cates.PicUrl = fileName;
+            db.T_Shop_Category.Add(cates);
+            db.SaveChanges();
             return View();
         }
     }
